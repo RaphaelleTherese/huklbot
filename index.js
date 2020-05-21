4b7884d00d508d17b0884324a194c183068db763
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2020-05-20 20:23:10 -0700

    WIP Gulagify

--- index.js
+++ index.js
@@ -38,6 +38,22 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 // const token = 'NDMzNDc5OTY0MDk3MTE4MjA4.Xn25-A.AejV_FMOQtZCZegt9OKK2bbq48w';
 const PREFIX = "~";
+const imageapi = require("imageapi.js");
+module.exports={
+    name: "huklbot", 
+    description: "Indescribable", 
+    category: "cool", 
+    run: async(bot, message, args) => {
+        let subreddits = [
+            "cursedimages",
+            "popping"
+        ]
+        let subreddit = subreddits[Math.floor(Math.random() * subreddits.length)]
+        console.log(subreddit);
+        let img = await api(subreddit);
+        console.log(subreddit);
+    }
+}
 
 const huklbot = "<@433479964097118208>"; // Huklbot's role
 const guild = bot.guilds.cache.get("456235847529005078"); // The server
