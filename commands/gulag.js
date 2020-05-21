a38d0f1d8b68ec332569cb7c4ea890810c82e397
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2020-05-20 22:27:01 -0700

    Gulag WIP
    Moved functions

--- commands/gulag.js
+++ commands/gulag.js
@@ -1,9 +1,56 @@
 const discord = require('discord.js');
 const api = require('imageapi.js');
 
+const guild = bot.guilds.cache.get("456235847529005078"); // The server
+const gulagRoleId = "504552560493854731";
+const gulagChannelId = "504553057560821780";
+
 module.exports.run = async(isAdmin, bot, msg, args) => {
-    msg.reply("reached");
+    sendToGulag(isAdmin, msg, args);
+    
 };
+
+function sendToGulag(isAdmin, msg, args){
+    if (isAdmin){
+        const gulag = msg.guild.roles.cache.get(gulagRoleId); // Gulag role
+        const gulagChannel = msg.guild.channels.cache.find(ch => ch.id === gulagChannelId); // Gulag channel
+        if (args.length > 1 && args[1].toString().includes("<@") && (args[1].length == 21 || args[1].length == 22)){
+            var member = msg.mentions.members.first();
+            var prevRoles = [""];
+
+            if (!member.roles.cache.some(r=>["Admin"].includes(r.name))){
+                member.roles.cache.forEach(function(role) {
+                    if (role.name != "@everyone"){
+                        member.roles.remove(role.id);
+                        prevRoles.push(role.id)
+                    }
+                });
+
+                dictGulag[member.id] = prevRoles;
+                member.roles.add(gulag);
+                msg.reply("<@" + member + "> has been gulaged <:blush:681648731837169664>");
+                bot.users.cache.get(member.id).send("You've been gulaged <:evil:573737708099338250>");
+                gulagify(msg, member);
+                return;
+            } 
+            msg.reply("Sry, but ai don't have the poer for this <:cry:570001747809009674>");
+            return;
+        } 
+        msg.reply("You have to provide a valid member to send to gulag! <:ss:456282514068340756>");
+        return;
+    }
+    msg.reply("Sry, but ur weak. You lack the poer! <:ss:456282514068340756>");
+}
+
+function gulagify(msg, member){
+    //Have a local or global variable set to the safe word 
+    //Allow admin to set the safe word
+    // Initiate url fetch 
+
+    const gulagChannel = msg.guild.channels.cache.find(ch => ch.id === gulagChannelId);
+    gulagChannel.send("<@" + member + "> The gulag process will start now <:evil:573737708099338250>");
+
+}
 
 module.exports.help = {
     name: "gulag"
--- index.js
+++ index.js
@@ -183,50 +183,6 @@ function displayProfile(msg, args){
     msg.channel.send(embed);
 }
 
-    // GULAG COMMANDS
-function sendToGulag(isAdmin, msg, args){
-    if (isAdmin){
-        const gulag = msg.guild.roles.cache.get(gulagRoleId); // Gulag role
-        const gulagChannel = msg.guild.channels.cache.find(ch => ch.id === gulagChannelId); // Gulag channel
-        if (args.length > 1 && args[1].toString().includes("<@") && (args[1].length == 21 || args[1].length == 22)){
-            var member = msg.mentions.members.first();
-            var prevRoles = [""];
-
-            if (!member.roles.cache.some(r=>["Admin"].includes(r.name))){
-                member.roles.cache.forEach(function(role) {
-                    if (role.name != "@everyone"){
-                        member.roles.remove(role.id);
-                        prevRoles.push(role.id)
-                    }
-                });
-
-                dictGulag[member.id] = prevRoles;
-                member.roles.add(gulag);
-                msg.reply("<@" + member + "> has been gulaged <:blush:681648731837169664>");
-                bot.users.cache.get(member.id).send("You've been gulaged <:evil:573737708099338250>");
-                gulagify(msg, member);
-                return;
-            } 
-            msg.reply("Sry, but ai don't have the poer for this <:cry:570001747809009674>");
-            return;
-        } 
-        msg.reply("You have to provide a valid member to send to gulag! <:ss:456282514068340756>");
-        return;
-    }
-    msg.reply("Sry, but ur weak. You lack the poer! <:ss:456282514068340756>");
-}
-
-function gulagify(msg, member){
-    //Have a local or global variable set to the safe word 
-    //Allow admin to set the safe word
-    // Initiate url fetch 
-
-
-    const gulagChannel = msg.guild.channels.cache.find(ch => ch.id === gulagChannelId);
-    gulagChannel.send("<@" + member + "> The gulag process will start now <:evil:573737708099338250>");
-
-}
-
 function removeFromGulag(isAdmin, msg, args){
     if (isAdmin){
         const gulag = msg.guild.roles.cache.get(gulagRoleId); // Gulag role
