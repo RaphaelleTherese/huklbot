38ed92f9fd7fcd24349397e40325a0fcccab8002
RTGuinanao <54865442+RTGuinanao@users.noreply.github.com>
2020-09-01 15:26:52 -0700

    Implementing harvesting functionality

--- commands/help.js
+++ commands/help.js
@@ -9,14 +9,31 @@ module.exports.run = async(bot, msg, args) => {
     if (args[1] == "me"){
         msg.reply("You need all the help you can get :unamused:");
     } else{
-      const embed = new Discord.MessageEmbed()
-      .setTitle('__COMMANDS__')
-      .setColor('#b72025')
-      .setDescription('Listen to me you little shit, here are your fucking commands, happy now?\n\n ' + 
-      '`help` Dumbass, you just used this \n ' + 
-      '`bark`\tWoof woof, motherfucker')
-      .addField('profile', 'View your profile')
-      .addField('command', 'command description');
+        const embed = new Discord.MessageEmbed()
+        .setTitle('__COMMANDS__')
+        .setColor('#b72025')
+        .setDescription('Listen to me you little shit, here are your fucking commands, happy now?\n\n ' + 
+            '`help` Dumbass, you just used this \n ' + 
+            '`bark`\tWoof woof, motherfucker')
+        .addField('FUN', '' + 
+          '`bark` Barks at the user sending the most recent message (HUKL ONLY <:smug:570002038318956544>)\n' +
+          '')
+        .addField('GULAG', '`~gulag <arg>`\n' + 
+            '`send <@user>` Send a desired user to the depths of gulag\n' +
+            '`release <@user>` Save a desired user from the gloomy gulag (for SIMPS only)\n' + 
+            '')
+        .addField('HARVEST', '`~harvest/~h <arg>`\n' + 
+            '`spawnorgan <organ> <emoji>` Spawns a new organ for each user. (HUKL ONLY <:smug:570002038318956544>)\n' + 
+            '`listorgans` Lists all of the available organs\n' + 
+            '`myorgans` View the organs you\'ve collected\n' + 
+            '`reset` Resets the progress all across the board, giving everyone their organs back (HUKL ONLY <:smug:570002038318956544>)\n' + 
+            '`rm <organ name>` Removes an organ from everyone\'s organ collection (HUKL ONLY <:smug:570002038318956544>)\n' + 
+            '`top` View the leaderboard \n' +
+            '`donorcard` View your organ donor card (profile) \n')
+        .addField('ANNOY', '`~annoy <arg> `\n' + 
+            '`<@user> message` Sends given user a message\n' + 
+            '`everyone message` Sends everyone a message (ADMIN ONLY)')
+        .addField('OTHER', 'WIP');
 
       msg.channel.send(embed);
     }
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 Author: Raphaelle Guinanao
 Last Updated: May 20, 2020
 
+node . to turn on 
+
 *IDEAS
 ~insult: insults a random person on the server with a random insult
 ~organ battle, instead of pet teams and such; sell to the black market; 
@@ -10,6 +12,19 @@ Last Updated: May 20, 2020
 ~distort: makes everything weird
 ~gulagify: sends user to gulag, can only be used by admin
 
+black market
+specialize in certain people, ex. majesty is rarer
+organize myorgans display
+collect the people -- something special? 
+
+coins and xp system? get items like butt plugs, orca dildos
+
+collect items and 
+
+obscure currencies
+
+quest - bingo cards: randomly generated each day. Gotta collect 
+
 Rankings determined by how many times someone's talked, and have
 names such as:
 Baby Hippo
@@ -17,12 +32,23 @@ Turd Burglar
 Baby Snatcher
 etc
 
+Selling to the blackmarket puts the organs into their own place? Freezer? Storage? Devalues them? Harder to get back? 
+
+Currency? - Buy things
+
+Build your own prostitution circle? 
+Feeding your pet organs lets them 
+
+Daily Bingo Cards (collecting organs) gets you certain perks (temporary boosts?)
+
+
 ------ USER @s -----
 <@433479964097118208> = HuklBot
 <@282624296637956108> = Huklpopz [Me]
 
 Notes:
 guild.roles.cache.forEach(role => console.log(role.name, role.id)) -- Retrieve all roles in the server
+
 */
 
 /*----- GLOBAL VARIABLES-----*/
@@ -32,21 +58,32 @@ const fs = require("fs");
 const mysql = require("mysql");
 
 const PREFIX = "~";
-const huklbot = "<@433479964097118208>"; // Huklbot's role
+const huklbot = "433479964097118208"; // Huklbot's role
+const me = "282624296637956108";
 const guild = bot.guilds.cache.get("456235847529005078"); // The server
 const gulagRoleId = "504552560493854731";
 const gulagChannelId = "504553057560821780";
 
 const token = "NDMzNDc5OTY0MDk3MTE4MjA4.Xw0uxg.nR7xhaCEb8-orHXk71lMkqVPe-Y";
 
+/*----- CONNECT TO DATABSE -----*/
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root", 
+    password: "ZingBox25HUK!", 
+    database: "notitiacentrum"
+});
+
+conn.connect(err => {
+    if(err) throw err;
+
+    console.log("Connected to database! OwO");
+});
+
 // Hey, is this thing on? 
 bot.on('ready', () =>{
     console.log("---------- Teehee, I'm hewe! UwU ----------");
 });
-
-var recentAuthor = "";
-var dictGulag = new Object();
-var safeWord = "safe word";
 
 // Load commands
 bot.commands = new Discord.Collection();
@@ -76,19 +113,23 @@ bot.on('guildMemberAdd', member => {
     channel.send(addIntro[randInt] + `${member}`);
 });
 
+/*----- GLOBAL VARIABLES -----*/
+var recentAuthor = "";
+var dictGulag = new Object();
+var safeWord = "safe word";
+
 // Message event
-bot.on('message', msg=>{
-     console.log(msg.author.username + ": " + msg.content.toLowerCase());
+bot.on('message', async msg=>{
+     console.log(msg.author.username + ": " + msg.content.toString().toLowerCase());
      var isAdmin = checkAdmin(msg);
     
-     if (msg.author.toString().includes(huklbot))
-        return;
-
-     if(!msg.author.toString().includes("<@282624296637956108>"))
-        recentAuthor = msg.author.toString(); // Retrieves the most recent 'author' of a message.
+     if (msg.author.bot) return;
+     if (msg.channel.type == "dm") return;
+     if(!msg.author.toString().includes(me)) recentAuthor = msg.author.toString(); // Retrieves the most recent 'author' of a message.
+    // if (msg.author.toString().includes("271065194438852618")) msg.channel.send("");
 
     // The bot obeys me UwU
-    if (msg.content.toLowerCase().includes("<@433479964097118208>")){
+    if (msg.content.toLowerCase().includes(huklbot)){
         if (msg.author.toString().includes("<@282624296637956108>")){
             msg.reply("At your service, master :hugging:");
         } else{
@@ -99,12 +140,11 @@ bot.on('message', msg=>{
     // --- OFFICIAL COMMANDS ---
     let cmd = "";
     let args = "";
-
-    if (msg.content.substring(0,PREFIX.length) == PREFIX)
-        args = msg.content.substring(PREFIX.length).toLowerCase().split(" ");
+    if (msg.content.substring(0,PREFIX.length) == PREFIX) args = msg.content.substring(PREFIX.length).toLowerCase().split(" ");
 
     cmd = bot.commands.get("gulag");
     cmd.run(bot, msg, args, isAdmin, "pester", dictGulag, safeWord);
+
     switch (args[0]){
          case "help":
              cmd = bot.commands.get("help");
@@ -127,10 +167,14 @@ bot.on('message', msg=>{
             } else if(isAdmin && args[1].toString() == "everyone") {
                 if (msg.content.substring(PREFIX.length + args[1].length).length > 0){
                     const guild = bot.guilds.cache.get("456235847529005078");
-                    guild.members.cache.forEach(function(member) {
+                    guild.members.cache.forEach(async function(member) {
+
+                        user = new Discord.GuildMember(bot, member, guild);
+                        
                         setTimeout(function(){
                             bot.users.cache.get(member.user.id).send(member.user.username + ", " + msg.content.substring(1 + PREFIX.length + args[0].toString().length + args[1].toString().length) + " - " + msg.author.toString())
                         }, 500); 
+
                     });
                 }
                 msg.reply(" I messaged them all, are you happy now? >:V");
@@ -154,11 +198,23 @@ bot.on('message', msg=>{
             }
             break;
         case "harvest":
+        case "h":
             cmd = bot.commands.get("harvest");
             cmd.run(bot, msg, args);
             break;
         case "test":
             msg.reply("This command is reserved for testing purposes. :)");
+            break;
+        case "ping":
+            var ping = Date.now() - msg.createdTimestamp + " ms";
+            msg.channel.send(new Discord.MessageEmbed()
+                .setColor('#b72025')
+                .setTitle(':information_source: Pong')
+                .setDescription(`My **ping** is **${Math.round(bot.ws.ping)}ms**.`)
+            );
+            break;
+        case "suggest":
+            suggest(msg, args);
             break;
      }
 });
@@ -179,5 +235,14 @@ function displayProfile(msg, args){
     msg.channel.send(embed);
 }
 
-bot.login(process.env.token);
-// bot.login(token); 
+function suggest(msg, args) {
+    var suggestion = msg.content.substring(PREFIX.length + args[0].length);
+    conn.query(`CALL SendSuggestion('${msg.author.id}', '${suggestion}')`, (err, results) => {
+        if (err) return console.error(err);
+
+        msg.reply("*I sent the suggestion, why don't you do it yourself next time. :confounded:*");
+    })
+}
+
+// bot.login(process.env.token);
+bot.login(token); 
